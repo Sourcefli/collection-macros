82eda5ac6eb7a86a2b1555d408efd3f1ae545f27
Sourcefli <jh@sourcefli.com>
2021-02-20 02:03:50 -0800

    slim service provider

--- src/CollectionMacrosServiceProvider.php
+++ src/CollectionMacrosServiceProvider.php
@@ -7,23 +7,6 @@ use Illuminate\Support\ServiceProvider;
 
 class CollectionMacrosServiceProvider extends ServiceProvider
 {
-    /**
-     * Perform post-registration booting of services.
-     *
-     * @return void
-     */
-    public function boot(): void
-    {
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'sourcefli');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'sourcefli');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
-        // Publishing is only necessary when using the CLI.
-        if ($this->app->runningInConsole()) {
-            $this->bootForConsole();
-        }
-    }
 
     /**
      * Register any package services.
